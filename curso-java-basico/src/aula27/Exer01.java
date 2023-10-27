03d0b1a64cd99806190161670b2cde27fb97c231
Rebeka Sena <bekacristine@gmail.com>
2023-10-27 10:09:14 -0300

    aula 27 exercicio 01

---
+++ curso-java-basico/src/aula27/Exer01.java
@@ -0,0 +1,20 @@
+package aula27;
+
+public class Exer01 {
+
+    public static void main(String[] args){
+        Lampada lampada = new Lampada();
+
+        lampada.ligar();
+
+        lampada.mostrarEstado();
+
+        lampada.desligar();
+
+        lampada.mostrarEstado();
+
+        lampada.mudarEstado();
+
+        lampada.mostrarEstado();
+    }
+}
