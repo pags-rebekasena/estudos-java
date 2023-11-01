cb7ade93c2772a8b671972eaf313a65c22dabfb7
Rebeka Sena <bekacristine@gmail.com>
2023-11-01 11:12:38 -0300

    Exercicio 01

---
+++ curso-java-basico/src/exerciciosJava/BalancoTrimestral.java
@@ -0,0 +1,16 @@
+package exerciciosJava;
+
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+
+        int gastosTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        System.out.println("Gastos do trimestre foi : " + gastosTrimestral);
+
+        int mediaMensal = gastosTrimestral/3;
+        System.out.println("A média salarial é : " + mediaMensal);
+    }
+}
